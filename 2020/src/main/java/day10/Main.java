c8d43791a00f1045e582516f7ce8f3158c8a68a6
Bradley Jones <jones.ngo@gmail.com>
2020-12-10 21:58:29 -0800

    Day 10 + Day 11 Solutions

--- 2020/src/main/java/day10/Main.java
+++ 2020/src/main/java/day10/Main.java
@@ -1,7 +1,83 @@
 package day10;
 
+import java.util.*;
+import java.io.*;
+import java.math.BigInteger;
+
+import lombok.*;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello from day10");
+
+
+    public static int partA(List<Integer> numbers) {
+        int joltsInput = 0;
+        int diff1 = 0;
+        int diff3 = 0;
+        for(int i = 0;i < numbers.size();++i) {
+            int adapter = numbers.get(i);
+            int diff = adapter - joltsInput;
+            if(diff < 0 || diff > 3) {
+                throw new RuntimeException(String.format("What? %d %d", joltsInput, adapter));
+            }
+            if(diff == 1) {
+                diff1 += 1;
+            }
+            if(diff == 3) {
+                diff3 += 1;
+            }
+            joltsInput = adapter;
+        }
+        diff3 += 1;
+        return diff1 * diff3;
+    } 
+
+    @Value
+    public static class Entry {
+        int inputVoltage;
+        int index;
+    }
+
+    public static BigInteger partBRecursive(List<Integer> numbers, int inputVoltage, int index, Map<Entry, BigInteger> memo) {
+        Entry newEntry = new Entry(inputVoltage,index);
+        BigInteger v = memo.get(newEntry);
+        if(v != null) {
+            return v;
+        }
+        int adapter = numbers.get(index);
+        if(adapter - inputVoltage > 3) {
+            memo.put(newEntry, BigInteger.ZERO);
+            return memo.get(newEntry);
+        }
+        if((index+1) == numbers.size()) {
+            memo.put(newEntry, BigInteger.ONE);
+            return memo.get(newEntry);
+        }
+        BigInteger retVal = BigInteger.ZERO;
+        for(int i = index+1;i < numbers.size();++i) {
+            retVal = retVal.add(partBRecursive(numbers, adapter, i, memo));    
+        }
+        memo.put(newEntry, retVal);
+        return memo.get(newEntry);
+    }
+
+    public static BigInteger partB(List<Integer> numbers) {
+        Map<Entry, BigInteger> memo = new HashMap<>();
+        BigInteger retVal = BigInteger.ZERO;
+        for(int i = 0;i < numbers.size();++i) {
+            retVal = retVal.add(partBRecursive(numbers, 0, i, memo));    
+        }
+        return retVal;
+    }
+
+    public static void main(String[] args) throws IOException {
+        List<Integer> numbers = new ArrayList<>();
+        try(val br = new BufferedReader(new InputStreamReader(Main.class.getClassLoader().getResourceAsStream("day10.txt")))) {
+            for(String line = br.readLine();line != null;line = br.readLine()) {
+                numbers.add(Integer.parseInt(line));       
+            }
+        }
+        Collections.sort(numbers);
+        System.out.println(String.format("Part A: %d", partA(numbers)));
+        System.out.println(String.format("Part B: %s", partB(numbers).toString()));
     }
 }
--- 2020/src/main/java/day11/Main.java
+++ 2020/src/main/java/day11/Main.java
@@ -1,7 +1,161 @@
 package day11;
 
+import java.util.*;
+import java.io.*;
+
+import lombok.*;
+
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello from day11");
+    public static enum SeatState {
+        OCCUPIED,
+        EMPTY 
+    }
+
+    @Value
+    public static class SeatLocation {
+        int x;
+        int y;
+    }
+
+    Map<SeatLocation, SeatState> seatStateMap = new HashMap<>();
+
+    public Main() {   
+    }
+
+    public void readInput() throws IOException {
+        try(val br = new BufferedReader(new InputStreamReader(Main.class.getClassLoader().getResourceAsStream("day11.txt")))) {
+            int y = 0;
+            for(String line = br.readLine();line != null;line = br.readLine()) {
+                for(int x = 0;x < line.length();++x) {
+                    char type = line.charAt(x);
+                    if(type == 'L') {
+                        seatStateMap.put(new SeatLocation(x, y), SeatState.EMPTY);
+                    }
+                    else if(type == '#') {
+                        seatStateMap.put(new SeatLocation(x, y), SeatState.OCCUPIED);
+                    }
+                }
+                ++y;
+            }
+        }
+    }
+
+    private int countOccupiedAdjacentSeats(Map<SeatLocation, SeatState> in, SeatLocation l) {
+        int count = 0;
+        for(int dx = -1;dx <= 1;++dx) {
+            for(int dy = -1;dy <= 1;++dy) {
+                if(dx == 0 && dy == 0) continue;
+                SeatState state = in.getOrDefault(new SeatLocation(l.getX()+dx, l.getY()+dy), SeatState.EMPTY);
+                if(state == SeatState.OCCUPIED) {
+                    ++count;
+                }
+            }
+        }
+        return count;
+    }
+
+    private boolean foundOccupiedSeat(Map<SeatLocation, SeatState> in, SeatLocation l,int dx,int dy) {
+        SeatLocation cl = new SeatLocation(l.getX()+dx, l.getY()+dy);
+
+        // TODO: Probably should have a way to drive this upper bound from the data
+        while((cl.getX() >= 0 && cl.getX() < 100) && (cl.getY() >= 0 && cl.getY() < 100)) {
+            if(in.containsKey(cl)) {
+                SeatState state = in.getOrDefault(cl, SeatState.EMPTY);
+                return state == SeatState.OCCUPIED;
+            }
+            cl = new SeatLocation(cl.getX()+dx, cl.getY()+dy);
+        }
+        return false;
+    }
+
+    private int countOccupiedAdjacentSeats2(Map<SeatLocation, SeatState> in, SeatLocation l) {
+        int count = 0;
+        for(int dx = -1;dx <= 1;++dx) {
+            for(int dy = -1;dy <= 1;++dy) {
+                if(dx == 0 && dy == 0) continue;
+                if(foundOccupiedSeat(in,l,dx,dy)) {
+                    ++count;
+                }
+            }
+        }
+        return count;
+    }
+
+    private Map<SeatLocation, SeatState> step(Map<SeatLocation, SeatState> in) {
+        Map<SeatLocation, SeatState> out = new HashMap<>();
+        in.entrySet().forEach(e -> {
+            int count = countOccupiedAdjacentSeats(in, e.getKey());
+            val state = e.getValue();
+            if(state == SeatState.EMPTY && count == 0) {
+                out.put(e.getKey(), SeatState.OCCUPIED);
+            }
+            else if(state == SeatState.OCCUPIED && count >= 4) {
+                out.put(e.getKey(), SeatState.EMPTY);
+            }
+            else {
+                out.put(e.getKey(), state);
+            }
+        });
+        return out;
+    }
+
+    private Map<SeatLocation, SeatState> step2(Map<SeatLocation, SeatState> in) {
+        Map<SeatLocation, SeatState> out = new HashMap<>();
+        in.entrySet().forEach(e -> {
+            int count = countOccupiedAdjacentSeats2(in, e.getKey());
+            val state = e.getValue();
+            if(state == SeatState.EMPTY && count == 0) {
+                out.put(e.getKey(), SeatState.OCCUPIED);
+            }
+            else if(state == SeatState.OCCUPIED && count >= 5) {
+                out.put(e.getKey(), SeatState.EMPTY);
+            }
+            else {
+                out.put(e.getKey(), state);
+            }
+        });
+        return out;
+    }
+
+    private int countOccupied(Map<SeatLocation, SeatState> current) {
+        int count = 0;
+        for(val entry : current.entrySet()) {
+            if(entry.getValue() == SeatState.OCCUPIED) {
+                ++count;
+            }
+        }
+        return count;
+    }
+
+    public String partA() {
+        Map<SeatLocation, SeatState> prev = seatStateMap;
+        Map<SeatLocation, SeatState> current = step(prev);
+
+        while(!current.equals(prev)) {
+            prev = current;
+            current = step(current);            
+        }
+
+        return Integer.toString(countOccupied(current));
+    }
+
+    public String partB() {
+        Map<SeatLocation, SeatState> prev = seatStateMap;
+        Map<SeatLocation, SeatState> current = step2(prev);
+
+        while(!current.equals(prev)) {
+            prev = current;
+            current = step2(current);   
+        }
+
+        return Integer.toString(countOccupied(current));
+    }
+
+    public static void main(String[] args) throws IOException {
+        val m = new Main();
+        m.readInput();
+        System.out.println(String.format("Part A: %s", m.partA()));
+        System.out.println(String.format("Part B: %s", m.partB()));
     }
 }
