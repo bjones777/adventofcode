2535f9b1627be62213551e5dc714077e2a09d0a8
Bradley Jones <jones.ngo@gmail.com>
2021-12-03 18:12:31 -0800

    Add Day04

---
+++ 2021/Day04/Day04.cpp
@@ -0,0 +1,14 @@
+#include <cassert>
+#include <cstdint>
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    std::cout << "Hello World!\n";
+	return 0;
+}
+
