153975e6d98d32de294b6b2a9466a5a11cf563b5
Bradley Jones <jones.ngo@gmail.com>
2022-10-25 22:20:04 -0700

    Move readlines into the aoc_2021 lib. Have day01 refer to that module.

--- rust/aoc_2021/src/bin/day01.rs
+++ rust/aoc_2021/src/bin/day01.rs
@@ -1,15 +1,5 @@
 use itertools::Itertools;
-use std::fs::File;
-use std::io::{self, BufRead};
-use std::path::Path;
-
-fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
-where
-    P: AsRef<Path>,
-{
-    let file = File::open(filename)?;
-    Ok(io::BufReader::new(file).lines())
-}
+use aoc_2021::*;
 
 fn part_a(meausrements: &Vec<i32>) -> i32 {
     meausrements
