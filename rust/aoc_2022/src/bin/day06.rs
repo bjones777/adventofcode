b3db47dddc893e0fb6efa5ae29e26d89327a8ee8
Bradley Jones <jones.ngo@gmail.com>
2022-11-16 02:26:11 -0800

    Setup AoC2022 skeleton

---
+++ rust/aoc_2022/src/bin/day06.rs
@@ -0,0 +1,18 @@
+use aoc_2022::*;
+
+fn part_a() -> i32 {
+    0
+}
+
+fn part_b() -> i32 {
+    0
+}
+
+fn main() {
+    let _lines: Vec<String> = read_lines("./day06.txt")
+        .unwrap()
+        .map(|s| s.unwrap())
+        .collect();
+    println!("Part A: {}", part_a());
+    println!("Part B: {}", part_b());
+}
