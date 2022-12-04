324d76ef741749a2533bebaa882684c7bfef12a7
Bradley Jones <jones.ngo@gmail.com>
2022-12-04 10:02:12 -0800

    move the tuples into their own variables, to stop t.0.0 madness.  Also clean up the slices, less magic numbers.

--- rust/aoc_2022/src/bin/day04.rs
+++ rust/aoc_2022/src/bin/day04.rs
@@ -1,46 +1,55 @@
 use aoc_2022::*;
 
-fn part_a(lines: &Vec<((i32,i32),(i32,i32))> ) -> i32 {
-    lines.iter().map(|t|
-        if (t.0.0 <= t.1.0 && t.0.1 >= t.1.1) || (t.1.0 <= t.0.0 && t.1.1 >= t.0.1) {
-            1
-        }
-        else {
-            0
-        }
-    ).sum()
+fn part_a(lines: &Vec<((i32, i32), (i32, i32))>) -> i32 {
+    lines
+        .iter()
+        .map(|t| {
+            let a = t.0;
+            let b = t.1;
+
+            if (a.0 <= b.0 && a.1 >= b.1) || (b.0 <= a.0 && b.1 >= a.1) {
+                1
+            } else {
+                0
+            }
+        })
+        .sum()
 }
 
-fn part_b(lines: &Vec<((i32,i32),(i32,i32))> ) -> i32 {
-    lines.iter().map(|t|
-        if (t.0.0 <= t.1.0 && t.0.1 >= t.1.0) || (t.1.0 <= t.0.0 && t.1.1 >= t.0.0) {
-            1
-        }
-        else {
-            0
-        }
-    ).sum()
+fn part_b(lines: &Vec<((i32, i32), (i32, i32))>) -> i32 {
+    lines
+        .iter()
+        .map(|t| {
+            let a = t.0;
+            let b = t.1;
+            if (a.0 <= b.0 && a.1 >= b.0) || (b.0 <= a.0 && b.1 >= a.0) {
+                1
+            } else {
+                0
+            }
+        })
+        .sum()
 }
 
 fn main() {
-    let lines: Vec<((i32,i32),(i32,i32))> = read_lines("./day04.txt")
+    let lines: Vec<((i32, i32), (i32, i32))> = read_lines("./day04.txt")
         .unwrap()
         .map(|s| s.unwrap())
         .map(|s| {
-            if let [r1,r2] = &s.split(",").collect::<Vec<&str>>()[0..2] {
-                if let [s1,e1] = &r1.split("-").collect::<Vec<&str>>()[0..2] {
-                    if let [s2,e2] = r2.split("-").collect::<Vec<&str>>()[0..2] {
-                        return ((s1.parse::<i32>().unwrap(), e1.parse::<i32>().unwrap()),(s2.parse::<i32>().unwrap(), e2.parse::<i32>().unwrap()));
-                    }
-                    else {
+            if let [r1, r2] = &s.split(",").collect::<Vec<&str>>()[..] {
+                if let [s1, e1] = &r1.split("-").collect::<Vec<&str>>()[..] {
+                    if let [s2, e2] = r2.split("-").collect::<Vec<&str>>()[..] {
+                        return (
+                            (s1.parse::<i32>().unwrap(), e1.parse::<i32>().unwrap()),
+                            (s2.parse::<i32>().unwrap(), e2.parse::<i32>().unwrap()),
+                        );
+                    } else {
                         panic!("couldn't split at 2 -");
                     }
-                }
-                else {
+                } else {
                     panic!("couldn't split at 1 -");
                 }
-            }
-            else {
+            } else {
                 panic!("couldn't split a ,");
             }
         })
