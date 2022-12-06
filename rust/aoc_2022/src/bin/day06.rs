661e3192e89c673eddd3b0d5c5de42d9d5272af3
Bradley Jones <jones.ngo@gmail.com>
2022-12-05 21:14:31 -0800

    day06 solution

--- rust/aoc_2022/src/bin/day06.rs
+++ rust/aoc_2022/src/bin/day06.rs
@@ -1,18 +1,46 @@
 use aoc_2022::*;
 
-fn part_a() -> i32 {
-    0
+fn has_matching(chs: &[char]) -> bool {
+    use std::collections::HashSet;
+    let mut set : HashSet<char> = HashSet::new();
+    for ch in chs {
+        if !set.insert(*ch) {
+            return true;
+        }
+    }
+    false
 }
 
-fn part_b() -> i32 {
-    0
+fn part_a(line: &String) -> usize {
+    let n = line.len();
+    let char_vec : Vec<char> = line.chars().collect();
+    for x in 0..(n-4) {
+        let chs = &char_vec[x..x+4];
+        if !has_matching(chs) {
+            return x+4;
+        }
+    }
+    panic!("Not found")
+}
+
+fn part_b(line: &String) -> usize {
+    let n = line.len();
+    let char_vec : Vec<char> = line.chars().collect();
+    for x in 0..(n-14) {
+        let chs = &char_vec[x..x+14];
+        if !has_matching(chs) {
+            return x+14;
+        }
+    }
+    panic!("Not found")
 }
 
 fn main() {
-    let _lines: Vec<String> = read_lines("./day06.txt")
+    let lines: Vec<String> = read_lines("./day06.txt")
         .unwrap()
         .map(|s| s.unwrap())
         .collect();
-    println!("Part A: {}", part_a());
-    println!("Part B: {}", part_b());
+    let line = lines.first().unwrap();
+    println!("Part A: {}", part_a(&line));
+    println!("Part B: {}", part_b(&line));
 }
