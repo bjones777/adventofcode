1a7356b149caf51224d3f379f863a9f2dd929dd1
Bradley Jones <jones.ngo@gmail.com>
2022-12-11 01:21:55 -0800

    fix some sillyness in day11 - just append the items to the working list instead of cloning, clearing - use num_traits zero as opposed to explicit to_bigint - remove bad try comment - use lazy static to stop rebuilding regex's

--- rust/aoc_2022/src/bin/day11.rs
+++ rust/aoc_2022/src/bin/day11.rs
@@ -1,6 +1,8 @@
 use aoc_2022::*;
 
+use lazy_static::lazy_static;
 use num_bigint::{BigInt, ToBigInt};
+use num_traits::Zero;
 use regex::Regex;
 
 enum Operation {
@@ -23,27 +25,31 @@ struct Monkey {
 }
 
 fn parse_operation(lines: &Vec<String>, line_num: usize) -> Operation {
-    let add_re = Regex::new(r".*Operation. new = old \+ (\d+)").unwrap();
-    let mul_re = Regex::new(r".*Operation. new = old \* (\d+)").unwrap();
-    let sqr_re = Regex::new(r".*Operation. new = old \* old").unwrap();
+    lazy_static! {
+        static ref ADD_RE: Regex = Regex::new(r".*Operation. new = old \+ (\d+)").unwrap();
+        static ref MUL_RE: Regex = Regex::new(r".*Operation. new = old \* (\d+)").unwrap();
+        static ref SQR_RE: Regex = Regex::new(r".*Operation. new = old \* old").unwrap();
+    }
     let line = &lines[line_num];
-    if let Some(cap) = add_re.captures(line) {
+    if let Some(cap) = ADD_RE.captures(line) {
         return Operation::Add(cap.get(1).unwrap().as_str().parse::<BigInt>().unwrap());
-    } else if let Some(cap) = mul_re.captures(line) {
+    } else if let Some(cap) = MUL_RE.captures(line) {
         return Operation::Mul(cap.get(1).unwrap().as_str().parse::<BigInt>().unwrap());
-    } else if let Some(_) = sqr_re.captures(line) {
+    } else if let Some(_) = SQR_RE.captures(line) {
         return Operation::Sqr;
     }
     panic!("Couldn't match operation");
 }
 
 fn parse_test(lines: &Vec<String>, line_num: usize) -> Test {
-    let div_re = Regex::new(r".*Test. divisible by (\d+)").unwrap();
-    let true_monkey_re = Regex::new(r".*If true. throw to monkey (\d+)").unwrap();
-    let false_monkey_re = Regex::new(r".*If false. throw to monkey (\d+)").unwrap();
-
+    lazy_static! {
+        static ref DIV_RE: Regex = Regex::new(r".*Test. divisible by (\d+)").unwrap();
+        static ref TRUE_MONKEY_RE: Regex = Regex::new(r".*If true. throw to monkey (\d+)").unwrap();
+        static ref FALSE_MONKEY_RE: Regex =
+            Regex::new(r".*If false. throw to monkey (\d+)").unwrap();
+    }
     Test {
-        divisible_by: div_re
+        divisible_by: DIV_RE
             .captures(&lines[line_num + 0])
             .unwrap()
             .get(1)
@@ -51,7 +57,7 @@ fn parse_test(lines: &Vec<String>, line_num: usize) -> Test {
             .as_str()
             .parse::<BigInt>()
             .unwrap(),
-        if_true: true_monkey_re
+        if_true: TRUE_MONKEY_RE
             .captures(&lines[line_num + 1])
             .unwrap()
             .get(1)
@@ -59,7 +65,7 @@ fn parse_test(lines: &Vec<String>, line_num: usize) -> Test {
             .as_str()
             .parse::<usize>()
             .unwrap(),
-        if_false: false_monkey_re
+        if_false: FALSE_MONKEY_RE
             .captures(&lines[line_num + 2])
             .unwrap()
             .get(1)
@@ -85,8 +91,10 @@ fn parse_starting_items(lines: &Vec<String>, line_num: usize) -> Vec<BigInt> {
 }
 
 fn parse_index(lines: &Vec<String>, line_num: usize) -> usize {
-    let index_re = Regex::new(r"Monkey (\d+).").unwrap();
-    index_re
+    lazy_static! {
+        static ref INDEX_RE: Regex = Regex::new(r"Monkey (\d+).").unwrap();
+    }
+    INDEX_RE
         .captures(&lines[line_num + 0])
         .unwrap()
         .get(1)
@@ -136,8 +144,8 @@ fn part_a(lines: &Vec<String>) -> usize {
     for _ in 0..20 {
         let mut new_items: Vec<Vec<BigInt>> = vec![vec![]; monkeys.len()];
         for monkey in &mut monkeys {
-            let mut items = monkey.items.clone();
-            monkey.items.clear();
+            let mut items: Vec<BigInt> = Vec::new();
+            items.append(&mut monkey.items);
             items.append(&mut new_items[monkey.index]);
             monkey_inspections[monkey.index] += items.len();
             for item in &items {
@@ -146,8 +154,7 @@ fn part_a(lines: &Vec<String>) -> usize {
                     Operation::Mul(x) => item * x,
                     Operation::Sqr => item * item,
                 });
-                if worry_level.clone() % monkey.test.divisible_by.clone() == 0.to_bigint().unwrap()
-                {
+                if worry_level.clone() % monkey.test.divisible_by.clone() == Zero::zero() {
                     new_items[monkey.test.if_true].push(worry_level);
                 } else {
                     new_items[monkey.test.if_false].push(worry_level);
@@ -175,8 +182,8 @@ fn part_b(lines: &Vec<String>) -> usize {
     for _ in 0..10000 {
         let mut new_items: Vec<Vec<BigInt>> = vec![vec![]; monkeys.len()];
         for monkey in &mut monkeys {
-            let mut items = monkey.items.clone();
-            monkey.items.clear();
+            let mut items: Vec<BigInt> = Vec::new();
+            items.append(&mut monkey.items);
             items.append(&mut new_items[monkey.index]);
             monkey_inspections[monkey.index] += items.len();
             for item in &items {
@@ -185,8 +192,7 @@ fn part_b(lines: &Vec<String>) -> usize {
                     Operation::Mul(x) => item * x,
                     Operation::Sqr => item * item,
                 };
-                if worry_level.clone() % monkey.test.divisible_by.clone() == 0.to_bigint().unwrap()
-                {
+                if worry_level.clone() % monkey.test.divisible_by.clone() == Zero::zero() {
                     new_items[monkey.test.if_true].push(worry_level % max_mod.clone());
                 } else {
                     new_items[monkey.test.if_false].push(worry_level % max_mod.clone());
@@ -211,7 +217,5 @@ fn main() {
         .collect();
 
     println!("Part A: {}", part_a(&lines));
-
-    // 27019168 too low
     println!("Part B: {}", part_b(&lines));
 }
