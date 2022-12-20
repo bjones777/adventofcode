67d5fc8a5364135a7f8c206f9dbd82f24f57a658
Bradley Jones <jones.ngo@gmail.com>
2022-12-20 01:38:02 -0800

    reformat day20

--- rust/aoc_2022/src/bin/day20.rs
+++ rust/aoc_2022/src/bin/day20.rs
@@ -1,6 +1,6 @@
 use aoc_2022::*;
 
-fn mix(encrypted : &mut Vec<(i32,usize)>, i : usize) {
+fn mix(encrypted: &mut Vec<(i32, usize)>, i: usize) {
     let mut src_index = 0;
     loop {
         if encrypted[src_index].1 == i {
@@ -11,51 +11,47 @@ fn mix(encrypted : &mut Vec<(i32,usize)>, i : usize) {
     let n = encrypted.len();
     let mut num_moves = encrypted[src_index].0;
     if num_moves >= 0 {
-        num_moves %= (n-1) as i32;
-    }
-    else {
+        num_moves %= (n - 1) as i32;
+    } else {
         num_moves = -num_moves;
-        num_moves %= (n-1) as i32;
+        num_moves %= (n - 1) as i32;
         num_moves = -num_moves;
     }
     while num_moves != 0 {
         if num_moves < 0 {
             if src_index == 0 {
                 let v = encrypted[src_index];
-                for i in 0..n-1 {
-                    encrypted[i] = encrypted[i+1];
+                for i in 0..n - 1 {
+                    encrypted[i] = encrypted[i + 1];
                 }
-                encrypted[n-1] = v;
-                encrypted.swap(n-1,n-2);
-                src_index = n-2;
-            }
-            else {
-                encrypted.swap(src_index,src_index-1);
+                encrypted[n - 1] = v;
+                encrypted.swap(n - 1, n - 2);
+                src_index = n - 2;
+            } else {
+                encrypted.swap(src_index, src_index - 1);
                 src_index -= 1;
             }
             num_moves += 1;
-        }
-        else {
-            if src_index == n-1 {
-                encrypted.swap(0,n-1);
+        } else {
+            if src_index == n - 1 {
+                encrypted.swap(0, n - 1);
                 src_index = 0;
-            }
-            else {
-                encrypted.swap(src_index,src_index+1);
+            } else {
+                encrypted.swap(src_index, src_index + 1);
                 src_index += 1;
             }
             num_moves -= 1;
         }
-    }   
+    }
 }
 
 fn part_a(lines: &Vec<i32>) -> i32 {
-    let mut encrypted : Vec<(i32,usize)> = Vec::new();
+    let mut encrypted: Vec<(i32, usize)> = Vec::new();
     for t in lines.iter().enumerate() {
-        encrypted.push((*t.1,t.0));
+        encrypted.push((*t.1, t.0));
     }
     for i in 0..lines.len() {
-        mix(&mut encrypted,i);
+        mix(&mut encrypted, i);
     }
     let mut zero_index = 0;
     loop {
@@ -65,10 +61,12 @@ fn part_a(lines: &Vec<i32>) -> i32 {
         zero_index += 1;
     }
     let n = encrypted.len();
-    encrypted[(zero_index+1000) % n].0 + encrypted[(zero_index+2000) % n].0 + encrypted[(zero_index+3000) % n].0
+    encrypted[(zero_index + 1000) % n].0
+        + encrypted[(zero_index + 2000) % n].0
+        + encrypted[(zero_index + 3000) % n].0
 }
 
-fn mix2(encrypted : &mut Vec<(i64,usize)>, i : usize) {
+fn mix2(encrypted: &mut Vec<(i64, usize)>, i: usize) {
     let mut src_index = 0;
     loop {
         if encrypted[src_index].1 == i {
@@ -79,52 +77,48 @@ fn mix2(encrypted : &mut Vec<(i64,usize)>, i : usize) {
     let n = encrypted.len();
     let mut num_moves = encrypted[src_index].0;
     if num_moves >= 0 {
-        num_moves %= (n-1) as i64;
-    }
-    else {
+        num_moves %= (n - 1) as i64;
+    } else {
         num_moves = -num_moves;
-        num_moves %= (n-1) as i64;
+        num_moves %= (n - 1) as i64;
         num_moves = -num_moves;
     }
     while num_moves != 0 {
         if num_moves < 0 {
             if src_index == 0 {
                 let v = encrypted[src_index];
-                for i in 0..n-1 {
-                    encrypted[i] = encrypted[i+1];
+                for i in 0..n - 1 {
+                    encrypted[i] = encrypted[i + 1];
                 }
-                encrypted[n-1] = v;
-                encrypted.swap(n-1,n-2);
-                src_index = n-2;
-            }
-            else {
-                encrypted.swap(src_index,src_index-1);
+                encrypted[n - 1] = v;
+                encrypted.swap(n - 1, n - 2);
+                src_index = n - 2;
+            } else {
+                encrypted.swap(src_index, src_index - 1);
                 src_index -= 1;
             }
             num_moves += 1;
-        }
-        else {
-            if src_index == n-1 {
-                encrypted.swap(0,n-1);
+        } else {
+            if src_index == n - 1 {
+                encrypted.swap(0, n - 1);
                 src_index = 0;
-            }
-            else {
-                encrypted.swap(src_index,src_index+1);
+            } else {
+                encrypted.swap(src_index, src_index + 1);
                 src_index += 1;
             }
             num_moves -= 1;
         }
-    }   
+    }
 }
 
 fn part_b(lines: &Vec<i32>) -> i64 {
-    let mut encrypted : Vec<(i64,usize)> = Vec::new();
+    let mut encrypted: Vec<(i64, usize)> = Vec::new();
     for t in lines.iter().enumerate() {
-        encrypted.push(((*t.1 as i64)*811589153i64,t.0));
+        encrypted.push(((*t.1 as i64) * 811589153i64, t.0));
     }
     for _r in 0..10 {
         for i in 0..lines.len() {
-            mix2(&mut encrypted,i);
+            mix2(&mut encrypted, i);
         }
     }
     let mut zero_index = 0;
@@ -135,16 +129,16 @@ fn part_b(lines: &Vec<i32>) -> i64 {
         zero_index += 1;
     }
     let n = encrypted.len();
-    encrypted[(zero_index+1000) % n].0 + encrypted[(zero_index+2000) % n].0 + encrypted[(zero_index+3000) % n].0
+    encrypted[(zero_index + 1000) % n].0
+        + encrypted[(zero_index + 2000) % n].0
+        + encrypted[(zero_index + 3000) % n].0
 }
 
 fn main() {
     let lines: Vec<i32> = read_lines("./day20.txt")
         .unwrap()
         .map(|s| s.unwrap())
-        .map(|s| {
-            s.as_str().parse::<i32>().unwrap()
-        })
+        .map(|s| s.as_str().parse::<i32>().unwrap())
         .collect();
     println!("Part A: {}", part_a(&lines));
     println!("Part B: {}", part_b(&lines));
