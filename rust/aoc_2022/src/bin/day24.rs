eafe1280a81ff30a9e9c5ab4c16bd81507e7e410
Bradley Jones <jones.ngo@gmail.com>
2022-12-24 16:39:52 -0800

    day24 reformat + add 1

--- rust/aoc_2022/src/bin/day24.rs
+++ rust/aoc_2022/src/bin/day24.rs
@@ -1,13 +1,12 @@
 use aoc_2022::*;
 
-use std::collections::{VecDeque, HashSet};
+use std::collections::{HashSet, VecDeque};
 
-#[derive(Clone, Copy, PartialEq, Eq,)]
+#[derive(Clone, Copy, PartialEq, Eq)]
 enum TileType {
     Empty,
     Wall,
 }
-
 
 #[derive(Clone, Copy)]
 enum Dir {
@@ -19,52 +18,76 @@ enum Dir {
 
 #[derive(Clone, Copy)]
 struct Blizzard {
-    loc: (usize,usize),
+    loc: (usize, usize),
     dir: Dir,
 }
 
-fn move_blizzards(board : &Vec<Vec<TileType>>, blizzards: &Vec<Blizzard>) -> Vec<Blizzard> {
-    let mut moved_blizzards : Vec<Blizzard> = Vec::new();
+fn move_blizzards(board: &Vec<Vec<TileType>>, blizzards: &Vec<Blizzard>) -> Vec<Blizzard> {
+    let mut moved_blizzards: Vec<Blizzard> = Vec::new();
     for blizzard in blizzards {
         match blizzard.dir {
             Dir::East => {
-                if board[blizzard.loc.0][blizzard.loc.1+1] == TileType::Wall {
-                    moved_blizzards.push(Blizzard { loc: (blizzard.loc.0,1), dir: blizzard.dir });
+                if board[blizzard.loc.0][blizzard.loc.1 + 1] == TileType::Wall {
+                    moved_blizzards.push(Blizzard {
+                        loc: (blizzard.loc.0, 1),
+                        dir: blizzard.dir,
+                    });
+                } else {
+                    moved_blizzards.push(Blizzard {
+                        loc: (blizzard.loc.0, blizzard.loc.1 + 1),
+                        dir: blizzard.dir,
+                    });
                 }
-                else {
-                    moved_blizzards.push(Blizzard { loc: (blizzard.loc.0, blizzard.loc.1+1), dir: blizzard.dir });
+            }
+            Dir::West => {
+                if board[blizzard.loc.0][blizzard.loc.1 - 1] == TileType::Wall {
+                    moved_blizzards.push(Blizzard {
+                        loc: (blizzard.loc.0, board[blizzard.loc.0].len() - 2),
+                        dir: blizzard.dir,
+                    });
+                } else {
+                    moved_blizzards.push(Blizzard {
+                        loc: (blizzard.loc.0, blizzard.loc.1 - 1),
+                        dir: blizzard.dir,
+                    });
                 }
-            },
-            Dir::West =>  {
-                if board[blizzard.loc.0][blizzard.loc.1-1] == TileType::Wall {
-                    moved_blizzards.push(Blizzard { loc: (blizzard.loc.0,board[blizzard.loc.0].len()-2), dir: blizzard.dir });
-                }
-                else {
-                    moved_blizzards.push(Blizzard { loc: (blizzard.loc.0, blizzard.loc.1-1), dir: blizzard.dir });
-                }
-            },
+            }
             Dir::South => {
-                if board[blizzard.loc.0+1][blizzard.loc.1] == TileType::Wall {
-                    moved_blizzards.push(Blizzard { loc: (2,blizzard.loc.1), dir: blizzard.dir });
+                if board[blizzard.loc.0 + 1][blizzard.loc.1] == TileType::Wall {
+                    moved_blizzards.push(Blizzard {
+                        loc: (2, blizzard.loc.1),
+                        dir: blizzard.dir,
+                    });
+                } else {
+                    moved_blizzards.push(Blizzard {
+                        loc: (blizzard.loc.0 + 1, blizzard.loc.1),
+                        dir: blizzard.dir,
+                    });
                 }
-                else {
-                    moved_blizzards.push(Blizzard { loc: (blizzard.loc.0+1, blizzard.loc.1), dir: blizzard.dir });
-                }
-            },
+            }
             Dir::North => {
-                if board[blizzard.loc.0-1][blizzard.loc.1] == TileType::Wall {
-                    moved_blizzards.push(Blizzard { loc: (board.len()-3,blizzard.loc.1), dir: blizzard.dir });
+                if board[blizzard.loc.0 - 1][blizzard.loc.1] == TileType::Wall {
+                    moved_blizzards.push(Blizzard {
+                        loc: (board.len() - 3, blizzard.loc.1),
+                        dir: blizzard.dir,
+                    });
+                } else {
+                    moved_blizzards.push(Blizzard {
+                        loc: (blizzard.loc.0 - 1, blizzard.loc.1),
+                        dir: blizzard.dir,
+                    });
                 }
-                else {
-                    moved_blizzards.push(Blizzard { loc: (blizzard.loc.0-1, blizzard.loc.1), dir: blizzard.dir });
-                }
-            },
+            }
         }
     }
     moved_blizzards
 }
 
-fn enqueue(in_q: &mut HashSet<((usize, usize), usize)>,queue: &mut VecDeque<((usize, usize), usize)>,new_state: ((usize, usize), usize)) {
+fn enqueue(
+    in_q: &mut HashSet<((usize, usize), usize)>,
+    queue: &mut VecDeque<((usize, usize), usize)>,
+    new_state: ((usize, usize), usize),
+) {
     if in_q.contains(&new_state) {
         return;
     }
@@ -72,12 +95,18 @@ fn enqueue(in_q: &mut HashSet<((usize, usize), usize)>,queue: &mut VecDeque<((us
     in_q.insert(new_state);
 }
 
-fn is_loc_safe(loc: (usize,usize), board : &Vec<Vec<TileType>>, blizzards_at_round: &mut Vec<Vec<Blizzard>>, blizzard_locs_at_round: &mut Vec<HashSet<(usize,usize)>>,round : usize) -> bool {
-    while round+1 > blizzards_at_round.len() {
+fn is_loc_safe(
+    loc: (usize, usize),
+    board: &Vec<Vec<TileType>>,
+    blizzards_at_round: &mut Vec<Vec<Blizzard>>,
+    blizzard_locs_at_round: &mut Vec<HashSet<(usize, usize)>>,
+    round: usize,
+) -> bool {
+    while round + 1 > blizzards_at_round.len() {
         blizzards_at_round.push(move_blizzards(board, blizzards_at_round.last().unwrap()))
     }
     while blizzard_locs_at_round.len() < blizzards_at_round.len() {
-        let mut locs : HashSet<(usize,usize)> = HashSet::new();
+        let mut locs: HashSet<(usize, usize)> = HashSet::new();
         for vb in blizzards_at_round[blizzard_locs_at_round.len()].iter() {
             locs.insert(vb.loc);
         }
@@ -89,107 +118,200 @@ fn is_loc_safe(loc: (usize,usize), board : &Vec<Vec<TileType>>, blizzards_at_rou
     ret_val
 }
 
-fn get_rounds_to_reach_dest(start_loc : (usize,usize),
-     end_loc: (usize,usize), from_round: usize, blizzards_at_round: &mut Vec<Vec<Blizzard>>, blizzard_locs_at_round: &mut Vec<HashSet<(usize,usize)>>
-    , board : &Vec<Vec<TileType>>) -> usize {
+fn get_rounds_to_reach_dest(
+    start_loc: (usize, usize),
+    end_loc: (usize, usize),
+    from_round: usize,
+    blizzards_at_round: &mut Vec<Vec<Blizzard>>,
+    blizzard_locs_at_round: &mut Vec<HashSet<(usize, usize)>>,
+    board: &Vec<Vec<TileType>>,
+) -> usize {
     let mut in_q: HashSet<((usize, usize), usize)> = HashSet::new();
     let mut queue: VecDeque<((usize, usize), usize)> = VecDeque::new();
-    enqueue(&mut in_q, &mut queue, (start_loc,from_round));
+    enqueue(&mut in_q, &mut queue, (start_loc, from_round));
     while !queue.is_empty() {
         let state = queue.pop_front().unwrap();
         if state.0 == end_loc {
             return state.1 + 1;
         }
         let loc = state.0;
-        if is_loc_safe(state.0,&board,blizzards_at_round, blizzard_locs_at_round, state.1+1) {
-            enqueue(&mut in_q, &mut queue,(state.0,state.1+1));
+        if is_loc_safe(
+            state.0,
+            &board,
+            blizzards_at_round,
+            blizzard_locs_at_round,
+            state.1 + 1,
+        ) {
+            enqueue(&mut in_q, &mut queue, (state.0, state.1 + 1));
         }
-        if board[loc.0-1][loc.1] == TileType::Empty && is_loc_safe((loc.0-1,loc.1),&board,blizzards_at_round, blizzard_locs_at_round, state.1+1) {
-            enqueue(&mut in_q, &mut queue,((loc.0-1,loc.1), state.1+1));
+        if board[loc.0 - 1][loc.1] == TileType::Empty
+            && is_loc_safe(
+                (loc.0 - 1, loc.1),
+                &board,
+                blizzards_at_round,
+                blizzard_locs_at_round,
+                state.1 + 1,
+            )
+        {
+            enqueue(&mut in_q, &mut queue, ((loc.0 - 1, loc.1), state.1 + 1));
         }
-        if board[loc.0+1][loc.1] == TileType::Empty && is_loc_safe((loc.0+1,loc.1),&board,blizzards_at_round, blizzard_locs_at_round, state.1+1) {
-            enqueue(&mut in_q, &mut queue,((loc.0+1,loc.1), state.1+1));
+        if board[loc.0 + 1][loc.1] == TileType::Empty
+            && is_loc_safe(
+                (loc.0 + 1, loc.1),
+                &board,
+                blizzards_at_round,
+                blizzard_locs_at_round,
+                state.1 + 1,
+            )
+        {
+            enqueue(&mut in_q, &mut queue, ((loc.0 + 1, loc.1), state.1 + 1));
         }
-        if board[loc.0][loc.1+1] == TileType::Empty && is_loc_safe((loc.0,loc.1+1),&board,blizzards_at_round, blizzard_locs_at_round, state.1+1) {
-            enqueue(&mut in_q, &mut queue,((loc.0,loc.1+1), state.1+1));
+        if board[loc.0][loc.1 + 1] == TileType::Empty
+            && is_loc_safe(
+                (loc.0, loc.1 + 1),
+                &board,
+                blizzards_at_round,
+                blizzard_locs_at_round,
+                state.1 + 1,
+            )
+        {
+            enqueue(&mut in_q, &mut queue, ((loc.0, loc.1 + 1), state.1 + 1));
         }
-        if board[loc.0][loc.1-1] == TileType::Empty && is_loc_safe((loc.0,loc.1-1),&board,blizzards_at_round, blizzard_locs_at_round, state.1+1) {
-            enqueue(&mut in_q, &mut queue,((loc.0,loc.1-1), state.1+1));
+        if board[loc.0][loc.1 - 1] == TileType::Empty
+            && is_loc_safe(
+                (loc.0, loc.1 - 1),
+                &board,
+                blizzards_at_round,
+                blizzard_locs_at_round,
+                state.1 + 1,
+            )
+        {
+            enqueue(&mut in_q, &mut queue, ((loc.0, loc.1 - 1), state.1 + 1));
         }
     }
     panic!("Couldn't find path");
 }
 
 fn part_a(lines: &Vec<String>) -> usize {
-    let num_rows = lines.len()+2;
+    let num_rows = lines.len() + 2;
     let num_cols = lines.first().unwrap().len();
-    let mut board : Vec<Vec<TileType>> = vec![vec![TileType::Empty; num_cols]; num_rows];
+    let mut board: Vec<Vec<TileType>> = vec![vec![TileType::Empty; num_cols]; num_rows];
     for c in 0..num_cols {
         board[0][c] = TileType::Wall;
-        board[num_rows-1][c] = TileType::Wall;
+        board[num_rows - 1][c] = TileType::Wall;
     }
-    let mut blizzards : Vec<Blizzard> = vec![];
-    for r in 0..(num_rows-2) {
-        for (c,ch) in lines[r].chars().enumerate() {
+    let mut blizzards: Vec<Blizzard> = vec![];
+    for r in 0..(num_rows - 2) {
+        for (c, ch) in lines[r].chars().enumerate() {
             match ch {
-                '#' => board[r+1][c] = TileType::Wall,
-                '^' => blizzards.push(Blizzard { loc: (r+1, c), dir: Dir::North }),
-                '>' => blizzards.push(Blizzard { loc: (r+1, c), dir: Dir::East }),
-                '<' => blizzards.push(Blizzard { loc: (r+1, c), dir: Dir::West }),
-                'v' => blizzards.push(Blizzard { loc: (r+1, c), dir: Dir::South }),
-                '.' => {},
+                '#' => board[r + 1][c] = TileType::Wall,
+                '^' => blizzards.push(Blizzard {
+                    loc: (r + 1, c),
+                    dir: Dir::North,
+                }),
+                '>' => blizzards.push(Blizzard {
+                    loc: (r + 1, c),
+                    dir: Dir::East,
+                }),
+                '<' => blizzards.push(Blizzard {
+                    loc: (r + 1, c),
+                    dir: Dir::West,
+                }),
+                'v' => blizzards.push(Blizzard {
+                    loc: (r + 1, c),
+                    dir: Dir::South,
+                }),
+                '.' => {}
                 _ => panic!("Unknown char {}", ch),
             }
         }
     }
 
-    let start_loc = (1,1);
-    let end_loc = (lines.len()-1,lines.first().unwrap().len()-2);
-    
-    let mut blizzards_at_round:Vec<Vec<Blizzard>> = Vec::new();
+    let start_loc = (1, 1);
+    let end_loc = (lines.len() - 1, lines.first().unwrap().len() - 2);
+
+    let mut blizzards_at_round: Vec<Vec<Blizzard>> = Vec::new();
     blizzards_at_round.push(blizzards);
 
+    let mut blizzard_locs_at_round: Vec<HashSet<(usize, usize)>> = vec![];
 
-    let mut blizzard_locs_at_round: Vec<HashSet<(usize,usize)>> = vec![];
-    
-    get_rounds_to_reach_dest(start_loc,end_loc,0,&mut blizzards_at_round, &mut blizzard_locs_at_round, &board)
+    get_rounds_to_reach_dest(
+        start_loc,
+        end_loc,
+        0,
+        &mut blizzards_at_round,
+        &mut blizzard_locs_at_round,
+        &board,
+    )
 }
 
 fn part_b(lines: &Vec<String>) -> usize {
-    let num_rows = lines.len()+2;
+    let num_rows = lines.len() + 2;
     let num_cols = lines.first().unwrap().len();
-    let mut board : Vec<Vec<TileType>> = vec![vec![TileType::Empty; num_cols]; num_rows];
+    let mut board: Vec<Vec<TileType>> = vec![vec![TileType::Empty; num_cols]; num_rows];
     for c in 0..num_cols {
         board[0][c] = TileType::Wall;
-        board[num_rows-1][c] = TileType::Wall;
+        board[num_rows - 1][c] = TileType::Wall;
     }
-    let mut blizzards : Vec<Blizzard> = vec![];
-    for r in 0..(num_rows-2) {
-        for (c,ch) in lines[r].chars().enumerate() {
+    let mut blizzards: Vec<Blizzard> = vec![];
+    for r in 0..(num_rows - 2) {
+        for (c, ch) in lines[r].chars().enumerate() {
             match ch {
-                '#' => board[r+1][c] = TileType::Wall,
-                '^' => blizzards.push(Blizzard { loc: (r+1, c), dir: Dir::North }),
-                '>' => blizzards.push(Blizzard { loc: (r+1, c), dir: Dir::East }),
-                '<' => blizzards.push(Blizzard { loc: (r+1, c), dir: Dir::West }),
-                'v' => blizzards.push(Blizzard { loc: (r+1, c), dir: Dir::South }),
-                '.' => {},
+                '#' => board[r + 1][c] = TileType::Wall,
+                '^' => blizzards.push(Blizzard {
+                    loc: (r + 1, c),
+                    dir: Dir::North,
+                }),
+                '>' => blizzards.push(Blizzard {
+                    loc: (r + 1, c),
+                    dir: Dir::East,
+                }),
+                '<' => blizzards.push(Blizzard {
+                    loc: (r + 1, c),
+                    dir: Dir::West,
+                }),
+                'v' => blizzards.push(Blizzard {
+                    loc: (r + 1, c),
+                    dir: Dir::South,
+                }),
+                '.' => {}
                 _ => panic!("Unknown char {}", ch),
             }
         }
     }
 
-    let mut blizzards_at_round:Vec<Vec<Blizzard>> = Vec::new();
+    let mut blizzards_at_round: Vec<Vec<Blizzard>> = Vec::new();
     blizzards_at_round.push(blizzards);
 
+    let mut blizzard_locs_at_round: Vec<HashSet<(usize, usize)>> = vec![];
 
-    let mut blizzard_locs_at_round: Vec<HashSet<(usize,usize)>> = vec![];
+    let start_loc = (1, 1);
+    let end_loc = (lines.len() - 1, lines.first().unwrap().len() - 2);
 
-    let start_loc = (1,1);
-    let end_loc = (lines.len()-1,lines.first().unwrap().len()-2);
-    
-    let rounds_to_end = get_rounds_to_reach_dest(start_loc,end_loc,0,&mut blizzards_at_round, &mut blizzard_locs_at_round, &board);
-    let rounds_to_start =  get_rounds_to_reach_dest(end_loc, start_loc,rounds_to_end,&mut blizzards_at_round, &mut blizzard_locs_at_round, &board);
-    get_rounds_to_reach_dest(start_loc,end_loc,rounds_to_start+1,&mut blizzards_at_round, &mut blizzard_locs_at_round, &board)
+    let rounds_to_end = get_rounds_to_reach_dest(
+        start_loc,
+        end_loc,
+        0,
+        &mut blizzards_at_round,
+        &mut blizzard_locs_at_round,
+        &board,
+    );
+    let rounds_to_start = get_rounds_to_reach_dest(
+        end_loc,
+        start_loc,
+        rounds_to_end + 1,
+        &mut blizzards_at_round,
+        &mut blizzard_locs_at_round,
+        &board,
+    );
+    get_rounds_to_reach_dest(
+        start_loc,
+        end_loc,
+        rounds_to_start + 1,
+        &mut blizzards_at_round,
+        &mut blizzard_locs_at_round,
+        &board,
+    )
 }
 
 fn main() {
@@ -197,7 +319,7 @@ fn main() {
         .unwrap()
         .map(|s| s.unwrap())
         .collect();
-    
+
     println!("Part A: {}", part_a(&lines));
 
     // 722 too low
